63589bec1547aa098f67086280e03cad07047bd3
Airiddha <airiddha@gmail.com>
2024-07-15 17:52:59 +0530

    added in master

--- index.js
+++ index.js
@@ -47,6 +47,30 @@ server {
     res.status(500).send('Failed to perform the necessary operations.');
   }
 });
+app.post('/generate_caddy_config', async (req, res) => {
+  const { site_name, origin_location, origin_host } = req.body;
+
+  const nginxConfig = `
+  @app {
+    host ${site_name} www.${site_name}
+}
+reverse_proxy @app ${origin_location} {
+    header_up Host ${origin_host}
+}
+  `;
+
+  const fileName = `${site_name}`;
+  const filePath = path.join(nginxConfigPath, fileName);
+
+  try {
+    // Use async/await with promisified fs.writeFile
+    await writeFileAsync(filePath, nginxConfig);
+    res.status(200).send(`Nginx configuration file for ${site_name} generated, saved, and SSL certificate obtained.`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to perform the necessary operations.');
+  }
+});
 
 app.listen(3000, () => {
   console.log('Express server is running on port 3000');
